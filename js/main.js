4946a0a8e0e5f475a70e6a7653befcc46edd0e4d
Ayato Nakagami <ayato.nakagami@gmail.com>
2020-09-13 20:40:44 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,94 @@
+'use strict';
+
+{
+  const question = document.getElementById('question');
+  const choices = document.getElementById('choices');
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
+
+  const quizSet = shuffle([
+    {q: '世界で最も大きい湖は？', c: ['カスピ海', 'バイカル湖', 'オンタリオ湖', 'ネス湖', '琵琶湖']},
+    {q: '2の8乗は？', c: ['256', '4096', '8192', '8', '16']},
+    {q: '次のうち、最初にリリースされた言語は？', c: ['Python', 'JavaScript', 'HTML', 'C', 'Basic']},
+  ]);
+
+  // 何問目かを示すカウンタ
+  let currentNum = 0;
+
+  // 回答済みフラグ
+  let isAnswered;
+
+  // スコア
+  let score = 0;
+
+  function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+    return arr;
+  }
+
+  function checkAnswer(li) {
+    if (isAnswered) {
+      return;
+    }
+    isAnswered = true;
+
+    if (li.textContent === quizSet[currentNum].c[0]) {
+      // 正解
+      li.classList.add('correct');
+      score++;
+    } else {
+      // 不正解
+      li.classList.add('wrong');
+    }
+
+    btn.classList.remove('disabled');
+  }
+
+  function setQuiz() {
+    isAnswered = false;
+    question.textContent = quizSet[currentNum].q;
+
+    while(choices.firstChild) {
+      // choicesの子要素(liタグ)を順に消す
+      choices.removeChild(choices.firstChild);
+    }
+
+    const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+    shuffledChoices.forEach(choice => {
+      const li = document.createElement('li');
+      li.textContent = choice;
+      li.addEventListener('click', () => {
+        checkAnswer(li);
+      });
+      choices.appendChild(li);
+    });
+
+    if (currentNum === quizSet.length - 1) {
+      btn.textContent = 'Show Score';
+    }
+  }
+
+  setQuiz();
+
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('disabled')) {
+      return;
+    }
+    btn.classList.add('disabled');
+
+    if (currentNum === quizSet.length - 1) {
+      // スコア表示
+      // console.log(`Score: ${score} / ${quizSet.length}`);
+      scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+      result.classList.remove('hidden');
+    } else {
+      currentNum++;
+      setQuiz();
+    }
+  });
+
+}
